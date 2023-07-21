10ce4b03da40f04c6af86d31723f1a1d3821fa58
Lim-Juri <imjuly05@gmail.com>
2023-07-21 14:28:47 +0900

    first commit

--- app/src/main/java/com/example/calculator/Calculator2.kt
+++ app/src/main/java/com/example/calculator/Calculator2.kt
@@ -11,59 +11,10 @@ fun main() {
     val multiplyOperation = MultiplyOperation()
     val divideOperation = DivideOperation()
 
-    fun inputMyInfo(type:String):Any? {
-        return when(type) {
-            "a" -> {
-                println("숫자를 입력해주세요.")
-                while (true) {
-                    try {
-                        var origina:String? = readLine()
-                        return origina?.toInt() ?: -1
-                    } catch (e:Exception) {
-                        println("숫자를 다시 입력해주세요.")
-                    }
-                }
-            }
-            "op" -> {
-                println("연산자를 입력해주세요.")
-                while (true){
-                    try {
-                        var originop:String? = readLine()
-                        if (originop?.equals("+")==true ||originop?.equals("-")==true
-                            || originop?.equals("*")==true||originop?.equals("/")==true
-                        ){
-                            return originop
-                        }
-                        else {
-                            println("다른 연산자를 입력해주세요.")
-                        }
-                    } catch (e:Exception){
-                        println("다른 연산자를 입력해주세요.")
-                    }
-                }
-            }
-            "b" -> {
-                println("숫자를 입력해주세요.")
-                while (true){
-                    try {
-                        var originb:String? = readLine()
-                        return originb?.toInt()?:-1
-                    } catch (e:Exception){
-                        println("숫자를 다시 입력해주세요.")
-                    }
-                }
-            }
 
-            else -> {
-                return "no"
-            }
-        }
-    }
-
-
-    var a = inputMyInfo("a").toString().toInt()
-    var op = inputMyInfo("op").toString()
-    var b = inputMyInfo("b").toString().toInt()
+    var a = inputMyInfo1("a").toString().toInt()
+    var op = inputMyInfo1("op").toString()
+    var b = inputMyInfo1("b").toString().toInt()
 
 
     calculator.oper(addOperation)
@@ -74,6 +25,7 @@ fun main() {
     var mul = calculator.op(a, b)
     calculator.oper(divideOperation)
     var divi = calculator.op(a, b)
+
     var message = "계산이 끝났습니다."
 
 
@@ -96,5 +48,55 @@ fun main() {
 }
 
 fun end(string: String){
-    println("${String}")
+    println("${string}")
+}
+
+fun inputMyInfo1(type:String):Any? {
+
+    return when(type) {
+        "a" -> {
+            println("숫자를 입력해주세요.")
+            while (true) {
+                try {
+                    var origina:String? = readLine()
+                    return origina?.toInt() ?: -1
+                } catch (e:Exception) {
+                    println("숫자를 다시 입력해주세요.")
+                }
+            }
+        }
+        "op" -> {
+            println("연산자를 입력해주세요.")
+            while (true){
+                try {
+                    var originop:String? = readLine()
+                    if (originop?.equals("+")==true ||originop?.equals("-")==true
+                        || originop?.equals("*")==true||originop?.equals("/")==true
+                    ){
+                        return originop
+                    }
+                    else {
+                        println("다른 연산자를 입력해주세요.")
+                    }
+                } catch (e:Exception){
+                    println("다른 연산자를 입력해주세요.")
+                }
+            }
+        }
+        "b" -> {
+            println("숫자를 입력해주세요.")
+            while (true){
+                try {
+                    var originb:String? = readLine()
+                    return originb?.toInt()?:-1
+                } catch (e:Exception){
+                    println("숫자를 다시 입력해주세요.")
+                }
+            }
+        }
+
+        else -> {
+            return "no"
+        }
+    }
 }
